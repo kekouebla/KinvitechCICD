7c8129b73ef092848214a292dec9ce3d642cd73c
Kinvi Ekoue-Bla <blakinvi@gmail.com>
2018-09-14 08:53:44 -0400

    add HelloCircleCI

---
+++ src/HelloCircleCI/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloCircleCI
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app,
+            IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello CircleCI!");
+            });
+        }
+    }
+}
