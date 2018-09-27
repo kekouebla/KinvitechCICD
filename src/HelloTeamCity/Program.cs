7a87ceb07ea8f8fdabdf3d9e0c4a1dc049c098d3
Kinvi Ekoue-Bla <blakinvi@gmail.com>
2018-09-27 09:30:48 -0400

    Remove ignored files and add Jenkins, TeamCity, and TravisCI projects

---
+++ src/HelloTeamCity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloTeamCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
